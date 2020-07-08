280cf7e9eeeb67383bdc594262428f0d55fcf0cf
Michiel Valee <michiel@dockflow.be>
2020-07-08 22:21:25 +0200

    test files location

--- src/app.ts
+++ src/app.ts
@@ -10,13 +10,13 @@ graphyServer.register(DisplayCore);
 
 graphyServer.register(InfluxCore);
 
-new Orchestrator().execute(JSON.parse(fs.readFileSync('test_ss_1.txt').toString()));
-new Orchestrator().execute(JSON.parse(fs.readFileSync('test_ss_2.txt').toString()));
-new Orchestrator().execute(JSON.parse(fs.readFileSync('test_ss_3.txt').toString()));
-new Orchestrator().execute(JSON.parse(fs.readFileSync('test_ss_4.txt').toString()));
-new Orchestrator().execute(JSON.parse(fs.readFileSync('test_ss_5.txt').toString()));
-new Orchestrator().execute(JSON.parse(fs.readFileSync('test_ss_6.txt').toString()));
-new Orchestrator().execute(JSON.parse(fs.readFileSync('test_ss_7.txt').toString()));
-new Orchestrator().execute(JSON.parse(fs.readFileSync('test_ss_8.txt').toString()));
-new Orchestrator().execute(JSON.parse(fs.readFileSync('test_ss_9.txt').toString()));
-new Orchestrator().execute(JSON.parse(fs.readFileSync('test_ss_10.txt').toString()));
+new Orchestrator().execute(JSON.parse(fs.readFileSync('assets/test_files/test_ss_1.txt').toString()));
+new Orchestrator().execute(JSON.parse(fs.readFileSync('assets/test_files/test_ss_2.txt').toString()));
+new Orchestrator().execute(JSON.parse(fs.readFileSync('assets/test_files/test_ss_3.txt').toString()));
+new Orchestrator().execute(JSON.parse(fs.readFileSync('assets/test_files/test_ss_4.txt').toString()));
+new Orchestrator().execute(JSON.parse(fs.readFileSync('assets/test_files/test_ss_5.txt').toString()));
+new Orchestrator().execute(JSON.parse(fs.readFileSync('assets/test_files/test_ss_6.txt').toString()));
+new Orchestrator().execute(JSON.parse(fs.readFileSync('assets/test_files/test_ss_7.txt').toString()));
+new Orchestrator().execute(JSON.parse(fs.readFileSync('assets/test_files/test_ss_8.txt').toString()));
+new Orchestrator().execute(JSON.parse(fs.readFileSync('assets/test_files/test_ss_9.txt').toString()));
+new Orchestrator().execute(JSON.parse(fs.readFileSync('assets/test_files/test_ss_10.txt').toString()));
