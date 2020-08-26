1932567413d376c9c6b4c49a0cbf8caf3c870f27
Michiel Valee <michiel@dockflow.be>
2020-08-26 08:29:47 +0200

    console log

--- src/core/server.ts
+++ src/core/server.ts
@@ -43,8 +43,8 @@ export class GraphyServer {
             } catch (e) {
                 //
             }
-            console.error(new Date().toUTCString() + ' unhandledRejection:', err);
-            console.error(err);
+            console.log(new Date().toUTCString() + ' unhandledRejection:', err);
+            console.log(err);
             process.exit(1);
         });
         // Actual exit of process on uncaught error
@@ -54,8 +54,8 @@ export class GraphyServer {
             } catch (e) {
                 //
             }
-            console.error(new Date().toUTCString() + ' uncaughtException:', err.message);
-            console.error(err.stack);
+            console.log(new Date().toUTCString() + ' uncaughtException:', err.message);
+            console.log(err.stack);
             process.exit(1);
         });
     }
