20c707ee16793531bbd1a0eb200562a08ebc482a
Michiel Valee <michiel@dockflow.be>
2020-10-24 11:09:13 +0200

    include in script

--- run_local.sh
+++ run_local.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Starting"
 echo "Installing dependencies..."
-npm install
+npm install --legacy-peer-deps
 docker-compose up -d --remove-orphans
 echo "You can now start the dev server w/ 'npm run dev'"
